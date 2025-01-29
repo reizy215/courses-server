a29f130ffddcff202e8823b4324531764a2e4316
reizy215 <reizy215@gmail.com>
2025-01-29 18:25:32 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const userSchema = Schema({
     userName: { type: String, require: true },
     password: { type: String, require: true },
-    role: { type: String, require: true, default: "USER" },
+    role: { type: String, default: "USER" },
     registrationDate: { type: Date, default: new Date() },
     email: { type: String, require: true }
 })
